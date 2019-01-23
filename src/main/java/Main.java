6d39b0d6578c02db6d61a7e7dbe55d062572266d
Linus Eriksson <linusaxel@gmail.com>
2019-01-23 14:18:05 +0100

    Add main and Lanterna

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
+        Terminal terminal = terminalFactory.createTerminal();
+
+
+
+    }
+}
