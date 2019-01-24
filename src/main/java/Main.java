859937bd01b246c8df947300871c3a3a81716460
Your Name <you@example.com>
2019-01-24 10:33:10 +0100

    Create shapes and three new classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,38 @@
+import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.input.KeyStroke;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        //Setting up terminal
-        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
-        Terminal terminal = terminalFactory.createTerminal();
+        runTetris();
+    }
 
-        //Takes input as keystroke
-        KeyStroke keyStroke = terminal.pollInput();
+    private static void runTetris() throws IOException, InterruptedException {
+        TetrisConsole con = new TetrisConsole();
 
-        keyStroke = null;
+        Figure figL = new Figure(Shape.L_TETROMINO, TextColor.ANSI.MAGENTA);
+        Figure figO = new Figure(Shape.O_TETROMINO, TextColor.ANSI.YELLOW);
+        Figure figS = new Figure(Shape.S_TETRAOMINO, TextColor.ANSI.WHITE);
+        Figure figT = new Figure(Shape.T_TETROMINO, TextColor.ANSI.GREEN);
+        Figure figI = new Figure(Shape.I_TETROMINO, TextColor.ANSI.RED);
+
+
+        KeyStroke keyStroke;
         do {
-            Thread.sleep(5); // might throw InterruptedException
-            keyStroke = terminal.pollInput();
+            con.paintFigure(10, 15, figL);
+            con.paintFigure(20, 25, figO);
+            con.paintFigure(15, 20, figS);
+            con.paintFigure(26, 30, figT);
+            con.paintFigure(5, 5, figI);
+
+            Thread.sleep(1000);
+
+            keyStroke = con.pollInput();
         } while (keyStroke == null);
+        con.exit();
     }
 }
+
+
--- src/main/java/Shapes.java
+++
@@ -1,7 +0,0 @@
-public enum Shapes {
-    RED,
-    GREEN,
-    PINK,
-    WHITE,
-    YELLOW,
-}
