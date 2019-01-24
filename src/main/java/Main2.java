d17e59b0f6c5c38a3f44dfe7fab2c24f051c1747
Greg Kokkoris <grkokkoris@gmail.com>
2019-01-24 11:35:22 +0100

    this is gibberish

--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -1,40 +1,56 @@
 import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
-
+import static com.googlecode.lanterna.input.KeyType.*;
 import java.io.IOException;
 import java.security.Key;
 
 public class Main2 {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws Exception, IOException, InterruptedException {
         //Setting up terminal
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
         Terminal terminal = terminalFactory.createTerminal();
         terminal.setCursorVisible(false);
 
-        //Creates keystroke object, declares it as null
-        KeyStroke keyStroke = null;
+        while(true) {
+            //Creates keystroke object, declares it as null
+            KeyStroke keyStroke = null;
+            int x = 3;
+            int y = 3;
+            int counter = 0;
+            do {
+                Thread.sleep(5); // might throw InterruptedException
+                keyStroke = terminal.pollInput();
+                counter++;
+                if (counter % 50 == 0) {
+                    TetT tetT = new TetT(new Positions(x, y), terminal);
+                    tetT.printToTerminal(terminal, tetT.getPositions());
+                    terminal.flush(); // required one
+                    tetT.eraseFromTerminal(terminal, tetT.getPositions());
+                    y++;
+                }
+            } while (keyStroke == null);
+            //While no input from user, shape keeps going down
+            //goDown(terminal, keyStroke = terminal.pollInput());
 
-        //While no input from user, shape keeps going down
-        goDown(terminal, keyStroke = terminal.pollInput());
+            switch (keyStroke.getKeyType()) {
+                case ArrowUp:
 
-        switch (keyStroke.getKeyType()) {
-            case ArrowUp:
+                    break;
+                case ArrowDown:
 
-                break;
-            case ArrowDown:
+                    break;
+                case ArrowLeft:
+                    System.out.println("lskdjf");
+                    break;
+                case ArrowRight:
 
-                break;
-            case ArrowLeft:
-                System.out.println("lskdjf");
-                break;
-            case ArrowRight:
+                    break;
+            }
 
-                break;
+            goDown(terminal, keyStroke = terminal.pollInput());
         }
-
-        goDown(terminal, keyStroke = terminal.pollInput());
     }
 
     public static void goDown(Terminal terminal, KeyStroke keyStroke) throws IOException, InterruptedException {
